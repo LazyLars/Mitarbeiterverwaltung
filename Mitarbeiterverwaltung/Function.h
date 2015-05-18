d6569ce70fae3a83ff701e30a8cf7cf05087bee3
Lars Wallberg <wallberg.lars.net@gmail.com>
2015-05-18 16:56:38 +0200

    Revert "Hallo"
    This reverts commit bd665c6152bc31a2d2a39cb2131ef00c71498546.

--- Mitarbeiterverwaltung/Function.h
+++ Mitarbeiterverwaltung/Function.h
@@ -20,14 +20,13 @@ int MitarbeiterHinzufuegen(TMitarbeiter liste[], int anzahl,  TMitarbeiter neuer
 void MitarbeiterlisteAusgeben (TMitarbeiter liste[], int anzahl);
 
 // MitarbeiterNachIDSuchen
-void MitarbeiterNachIDSuchen(TMitarbeiter liste[], int anzahl);
 
 // SummeGehalt
-float SummeGehalt(TMitarbeiter liste[], int anzahl);
+
 // MaxMitarbeiterID
-int MaxMitarbeiterID(TMitarbeiter liste[], int anzahl);
+
 // SplitString
 
 // SortiereMitarbeiterNachGehalt
-void SortiereMitarbeiterNachGehalt(TMitarbeiter liste[], int anzahl);
+
 //Auswahlmenue
--- Mitarbeiterverwaltung/Main.cpp
+++ Mitarbeiterverwaltung/Main.cpp
@@ -17,29 +17,15 @@ int main (void)
 	//Einzellner Mitarbeiter
 	TMitarbeiter neuerMitarbeiter;
 	int anzahl = 0;
-	bool loop= false;
-	char answer;
 
-	while(!loop)
-	{
+
 	neuerMitarbeiter = MitarbeiterEinlesen();
 
 	anzahl = MitarbeiterHinzufuegen (mitarbListe, anzahl, neuerMitarbeiter);
 	
 	MitarbeiterlisteAusgeben (mitarbListe, anzahl);
 
-	//MitarbeiterNachIDSuchen(mitarbListe, anzahl);
-
 	system ("pause");
-	cout <<"Wiederholen?" <<endl;
-	cin >> answer;
-	if(answer == 'j')
-		loop = false;
-	else
-		loop = true;
-	}
-	SortiereMitarbeiterNachGehalt(mitarbListe,anzahl);
 
-	system ("pause");
 }
 
